2504246a982218b93f808f12891b4936742e4b0c
EladSella <162558956+EladSella@users.noreply.github.com>
2026-05-25 20:00:39 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,27 @@
+/* Ayala Guess Character - Service Worker v1 */
+const CACHE = 'ayala-v1';
+const PRECACHE = ['./', './index.html', './manifest.json'];
 
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE).catch(() => {})));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(ks =>
+      Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(r => {
+      if (r.ok && new URL(e.request.url).origin === self.location.origin) {
+        caches.open(CACHE).then(c => c.put(e.request, r.clone()));
+      }
+      return r;
+    }).catch(() => caches.match(e.request))
+  );
+});
